f0d58171502fa311ddbd973a02a38d9e401beba5
Biao WU <biaowu@BiaodeMacBook-Pro.local>
2025-02-14 13:33:17 +0800

    first commit

---
+++ src/main/java/com/vladosama/snake_cashier/SnakeCashierApplication.java
@@ -0,0 +1,13 @@
+package com.vladosama.snake_cashier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnakeCashierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnakeCashierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vladosama/snake_cashier/SnakeCashierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladosama.snake_cashier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakeCashierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
